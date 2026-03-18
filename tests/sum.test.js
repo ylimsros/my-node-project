32800695c35ae9c662bf0c062e50f309a1e3c607
ylimsros <ylimsros.pwd@gmail.com>
2026-03-18 16:08:11 +0700

    unfix: remove test minus

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 = 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('minus 5 - 2 = 3', () =>{
-  expect(minus(5, 2)). toBe(3);
-});
+// test('minus 5 - 2 = 3', () =>{
+//   expect(minus(5, 2)). toBe(3);
+// });
