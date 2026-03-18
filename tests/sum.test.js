7f80682239053366f05ccbc05e7d6967af5bca8f
ylimsros <ylimsros.pwd@gmail.com>
2026-03-18 15:44:29 +0700

    fix: test case minus

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,9 @@
-const sum = require('../src/sum');
+const {sum, minus} = require('../src/sum');
 
 test('adds 1 + 2 = 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('minus 5 - 2 = 3', () =>{
+  expect(minus(5, 2)). toBe(3);
 });
